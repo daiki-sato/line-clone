dad94244faad39cec298b72c84e459d568b42632
daiki-sato <daiki20010705@icloud.com>
2023-02-23 15:29:20 +0900

    feat: :wrench: Add firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyDcSmo131DRnUu08RlZUbWelcCl6j6_zfY",
+  authDomain: "line-clone-e20ba.firebaseapp.com",
+  projectId: "line-clone-e20ba",
+  storageBucket: "line-clone-e20ba.appspot.com",
+  messagingSenderId: "539715540541",
+  appId: "1:539715540541:web:24466f7be9888faefffc54",
+});
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
